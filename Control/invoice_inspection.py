3ba575ba9f2ab63f8933b7814b80f75d5db4b11e
Isac de Souza Campos <isacsouzacampos@hotmail.com>
2021-08-19 16:13:25 -0300

    Arquivos reorganizados.

---
+++ Control/invoice_inspection.py
@@ -0,0 +1,52 @@
+from openpyxl import Workbook
+from Model.constants import *
+from Model.invoice import Invoice
+from Model.excel_functions import upload_sheet_content
+from View.invoice_inspection import *
+
+
+def inspect_invoices(folder, xml_files) -> None:
+    """Cria um arquivo Excel contendo uma planilha com os dados referentes ao servico contido na nota"""
+
+    # cria o arquivo Excel a ser editado
+    excel_file = Workbook()
+
+    # gera uma planilha (sheet1)
+    sheet1 = excel_file.active
+
+    # insere o cabeçalho da planilha
+    # 'Nº Nota', 'Data', 'Valor Bruto', 'Retenção ISS',
+    # 'Retenção IR', 'Retenção CSRF', 'Valor Líquido', 'Natureza'
+    header = [COLUMN_TITLES[0], COLUMN_TITLES[1], COLUMN_TITLES[2], COLUMN_TITLES[3], COLUMN_TITLES[4],
+              COLUMN_TITLES[5], COLUMN_TITLES[6], COLUMN_TITLES[7]]
+
+    sheet1.append(header)
+
+    # lista que será passada como parâmetro para ser mostrada na tela pela GUI
+    results = list()
+
+    # inicia janela da barra de progresso da conferência
+    loading_window = start_loading_inspection_window()
+
+    # insere os dados de cada um dos arquivos xml a serem analisados
+    import time
+    for i in range(len(xml_files)):
+        invoice_file = xml_files[i]
+        invoice = Invoice(folder, invoice_file)
+        update_loading_window(loading_window, invoice.d['numeroserie'], i, len(xml_files))
+        row = invoice.data_list()
+        sheet1.append(row)
+        results.append(row)
+        time.sleep(.5)
+
+    loading_window.close()
+
+    show_results_table(header, results)
+    
+    upload_sheet_content(sheet1, xml_files)
+
+    # salva o arquivo Excel
+    excel_file.save(f'{folder.split("/")[-1]}.xlsx')
+
+    # abre o arquivo Excel gerado
+    # os.system(f'start excel.exe {folder.split("/")[-1]}.xlsx')
