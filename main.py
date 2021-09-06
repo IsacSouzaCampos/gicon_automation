642150c5f77f0be9c5ddb6b6cd085ca65cafca11
Isac de Souza Campos <isacsouzacampos@hotmail.com>
2021-09-06 15:06:34 -0300

    Adicionada função 'exit()' ao final do código para tentar contornar erro causado no executável.

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ import View.invoices_inspection as gui_invoice_inspection
 def main():
     folder, xml_files, service_type = gui_invoice_inspection.main_gui()
 
-    print(folder)
-    print(xml_files)
-    print(service_type)
+    # print(folder)
+    # print(xml_files)
+    # print(service_type)
 
     invoice_inspected = control_invoice_inspection.inspect_invoices(folder, xml_files, service_type)
 
     while not invoice_inspected:
         folder, xml_files, service_type = gui_invoice_inspection.main_gui()
         invoice_inspected = control_invoice_inspection.inspect_invoices(folder, xml_files, service_type)
+
+    exit()
 
 
 if __name__ == '__main__':
