f5ba15785440584552b41273410067f5f8389b64
Isac de Souza Campos <isacsouzacampos@hotmail.com>
2021-09-22 17:01:49 -0300

    Criada função de conexão com o BD.

---
+++ Model/sql.py
@@ -0,0 +1,22 @@
+import fdb
+# import sys
+
+
+def run_command(command):
+    try:
+        con = fdb.connect(
+            host='10.0.4.92', database=r'C:\Questor\db_questor\Saraiva_teste.FDB',
+            user='SYSDBA', password='masterkey'
+        )
+
+        # Create a Cursor object that operates in the context of Connection con:
+        cur = con.cursor()
+
+        # Execute the SELECT statement:
+        cur.execute(command)
+
+        con.close()
+        return cur
+
+    except Exception as e:
+        print(e)
