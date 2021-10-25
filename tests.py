cd35079353ea59653fa5dd3f80f342941edd4837
Isac de Souza Campos <isacsouzacampos@hotmail.com>
2021-10-25 09:55:27 -0300

    Código de testes atualizado de acordo com mudanças feitas.

--- tests.py
+++ tests.py
@@ -1,4 +1,10 @@
 from Control.inspection import inspect
+from Control.sql import SQLControl
+
+import Model.excel as excel
+import Model.constants as constants
+
+from View.inspection_lib import insertion_commands
 import os
 
 
@@ -20,7 +26,15 @@ def test():
 
     xml_files = [file for file in os.listdir(folder) if '.xml' in file]
 
-    inspect(folder, xml_files, service_type)
+    is_finished, invoices = inspect(folder, xml_files, service_type)
+
+    xlsx_file_name = folder.split('/')[-1] + '.xlsx'
+    excel.create_xlsx(constants.HEADER1, invoices, xlsx_file_name, xml_files)
+
+    sql_control = SQLControl(invoices)
+    commands = sql_control.run()
+
+    insertion_commands(commands)
 
 
 if __name__ == '__main__':
